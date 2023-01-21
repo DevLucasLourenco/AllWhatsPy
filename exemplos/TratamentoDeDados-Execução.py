ead42bf615288ee0c7cc9e650a9177b10a4c8a58
Lucas Lourenço <102922249+LucasLourencoo@users.noreply.github.com>
2023-01-21 16:14:33 -0300

    Add files via upload

---
+++ exemplos/TratamentoDeDados-Execução.py
@@ -0,0 +1,88 @@
+from AllWhatsPy import *
+import pandas as pd
+
+
+
+# Importação de Dados
+alunos_ativos1_df = pd.read_excel('planilha1.xlsx')
+alunos_ativos2_df = pd.read_excel('planilha2.xlsx')
+alunos_ativos3_df = pd.read_excel('planilha3.xlsx')
+
+
+
+# Junção de Dados
+planilha_df = alunos_ativos1_df.merge(alunos_ativos2_df, how='outer')
+planilha_df = planilha_df.merge(alunos_ativos3_df, how='outer')
+
+
+
+# Tratamento de Dados
+planilha_df['Nome'] = [str(each).title() for each in planilha_df['Nome']]
+planilha_df['Celular'] = [str(each).replace('(','').replace(')','').replace('-','') for each in planilha_df['Celular']]
+planilha_df = planilha_df.sort_values(by='Nome')
+planilha_df = planilha_df[['Nome','Email Nome','Celular']]
+
+
+
+# Execução
+conexao()
+
+
+for i, nome in enumerate(planilha_df['Nome']):
+       celular = planilha_df.iloc[i, 2]
+       
+       texto = f''' Olá, {nome}!
+       Estou passando para lembrar que hoje vence o pagamento do seu plano.
+       Para mais informações, estou aqui para ajudá-lo.
+       '''
+
+       
+       print(f'{i+1} - {nome} - {celular}')
+       encontrar_usuario(celular)
+       enviar_mensagem_paragrafada(texto)
+
+       
+desconectar()
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Dados
+# texto = ['test SELENIUM by coding on python','lalalalaal','testeee']
+
+# lista = ['21980095027',
+#          '21959061623'
+#         ]
+
+
+# nome_midia = 'IMG.jpeg'
+# midia_diretorio = os.path.realpath(nome_midia)
+
+# texto1 = '''
+# ola
+# sou o lucas
+# :D
+# '''
+
+# # Execução 
+# conexao()
+
+# for item in lista:
+    
+#     encontrar_usuario(item)
+#     enviar_mensagem_encadeada(texto)
+#     enviar_imagem(midia_diretorio)
+    
+
+# desconectar()
+
+
