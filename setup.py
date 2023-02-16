1961f654ce3277d53af7d22677e705daa8605aad
Lucas Lourenço <dev.lucaslourenco@gmail.com>
2023-02-15 21:15:56 -0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as arq:
+with open("README.md", "r") as arq:
     readme = arq.read()
 
 setup(name='allwhatspy_awp',
-    version='0.0.9',
+    version='0.0.11.3',
     license='MIT License',
     author='Lucas Lourenço',
     long_description=readme,
     long_description_content_type="text/markdown",
     author_email='dev.lucaslourenco@gmail.com',
-    keywords='whatsapp api bot mensagem automática analise de mensagem',
+    keywords='whatsapp api bot mensagem automática analise whatsappAPI',
     description=u'Whatsapp API - Envio de Mensagem Automática - PT-BR. ',
     packages=['AllWhatsPy'],
     install_requires=['webdriver-manager', 'selenium'],)
