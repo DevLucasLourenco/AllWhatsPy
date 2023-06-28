5ec94efa4aab0b5c20af016c634f1dd440f626bb
Lucas Lourenço <dev.lucaslourenco@gmail.com>
2023-06-28 10:43:54 -0300

    Update and rename __init__.py to __init__.py

--- New Patch (Alpha)/__init__.py
+++ New Patch (Alpha)/__init__.py
@@ -1,24 +1,31 @@
-class AllWhatsPy:
+class AllWhatsPy: 
     flag_conection = False
+    
     def __init__(self):
         self.lista_teste = list()
         self.lista_informacoes_contato_aberto = list()
-        self.generator = self.__informacoes_contato_aberto()
+        self.generator_info_contato_acessado = self.__informacoes_contato_acessado()
+        
         self.msg = AWPMensagem(self) # instanciado para acessar os métodos de envio de mensagem
         self.ctts = AWPContatos(self) # instanciado para acessar os métodos de acessar contatos
+        
         
     def testando(self):
         print('de awp executando em mensagem')
         
+        
     def conexao(self):
         self.flag_conection = True
 
-    def __informacoes_contato_aberto(self):
-        # while True:
-            xpath = ...
+
+    def __informacoes_contato_acessado(self):
+        xpath = ...
+        while True:
+            # Etapa 1
             dados = ... # drive.find_element(By.XPATH, xpath).text
             yield 1
             
+            # Etapa 2
             self.lista_informacoes_contato_aberto.append(dados)
             yield 2
         
@@ -26,7 +33,15 @@ class AllWhatsPy:
     def exportar_txt(objeto): # qual atributo a pessoa decidir
         ...
         
+        
+        
 class AWPMensagem(AllWhatsPy):
+    """Utilizado para o envio de mensagem
+
+    Args:
+        AllWhatsPy (_type_): _description_
+    """
+    
     def __init__(self, objeto):
         self.objeto_awp = objeto # usado para acessar os atributos
         print('mensagem')
@@ -36,18 +51,34 @@ class AWPMensagem(AllWhatsPy):
         
         
 class AWPContatos(AllWhatsPy):
+    """Utilizado para encontrar contatos
+
+    Args:
+        AllWhatsPy (_type_): _description_
+    """
+    
     def __init__(self, objeto):
         self.objeto_awp = objeto # usado para acessar os atributos
         print('contatos')
         print(self.flag_conection)
         ...
+        
+        
     def encontrar_contato(self):
         ...
-        next(self.generator)
+        next(self.generator_info_contato_acessado)
         
         ...
-        next(self.generator)
+        
+        next(self.generator_info_contato_acessado)
+    
+        
+    def __verificacao_existencia_contato(self):
+        ...
         
         
-awp = AllWhatsPy()
-print(awp.lista_teste)
+        
+        
+if __name__=="__main__":
+    awp = AllWhatsPy()
+    print(awp.lista_teste)
