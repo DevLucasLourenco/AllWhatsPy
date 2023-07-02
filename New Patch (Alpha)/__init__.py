7dbc09a03d5b878a65eb4d5a3c165fcc3e3e86e1
Lucas Lourenço <dev.lucaslourenco@gmail.com>
2023-07-02 08:29:47 -0300

    Update __init__.py

--- New Patch (Alpha)/__init__.py
+++ New Patch (Alpha)/__init__.py
@@ -50,7 +50,7 @@ class AWPMensagem(AllWhatsPy):
     """
     
     def __init__(self, objeto):
-        self.objeto_awp = objeto # usado para acessar os atributos
+        self.objeto_awp = objeto # usado para acessar os atributos; mudar isto. criar um método onde receba o valor e faça a relação de atribuição aos atributos, não realizar diretamente com o objeto_awp
         print('mensagem')
         super().testando()
         self.objeto_awp.lista_teste.append('a')
