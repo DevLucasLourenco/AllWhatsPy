78e99381fee42b055b9fefcb881f3d8459a02d47
Lucas Lourenço <dev.lucaslourenco@gmail.com>
2023-07-03 23:11:31 -0300

    Add files via upload

--- New Patch (Alpha)/teste.py
+++ New Patch (Alpha)/teste.py
@@ -18,12 +18,13 @@ with awp.criptografia(res, 3, 'd') as c:
     res2 = c.fetch()
     print(res2)
 
-# algum_cep = 00000000
-# localizacao = awp.msg.localizacao(algum_cep).fetch()
-# print(localizacao)
+algum_cep = 24754000
+localizacao = awp.msg.localizacao(algum_cep).fetch()
+print(localizacao)
 
 
 
 
 
 
+awp
