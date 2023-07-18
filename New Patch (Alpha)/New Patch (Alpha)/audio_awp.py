a4d0ce4edb729aa43e603882ce0ce9bd9687d02b
Lucas Lourenço <dev.lucaslourenco@gmail.com>
2023-07-18 19:59:35 -0300

    Delete New Patch (Alpha) directory

--- New Patch (Alpha)/New Patch (Alpha)/audio_awp.py
+++
@@ -1,9 +0,0 @@
-
-class AWPAudio():
-    """
-    Classe para os serviços de Áudio   
-    """
-
-    def __init__(self, objeto):
-        ...
-
--- New Patch (Alpha)/__init__.py
+++
@@ -1,6 +0,0 @@
-from awp import AllWhatsPy
-from mensagem_awp import AWPMensagem
-from contatos_awp import AWPContatos
-from criptografia_awp import AWPCriptografia
-from audio_awp import AWPAudio
-
