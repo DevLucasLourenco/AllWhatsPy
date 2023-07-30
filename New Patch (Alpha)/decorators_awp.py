b3eb17814da330ace570dc1ffaad72d56cefcf9b
Lucas Lourenço <dev.lucaslourenco@gmail.com>
2023-07-30 16:47:18 -0300

    Add files via upload

--- New Patch (Alpha)/decorators_awp.py
+++ New Patch (Alpha)/decorators_awp.py
@@ -1,10 +1,5 @@
 from errors_awp import *
 import time as t
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
 
 
 def aprovarConexao(func):
