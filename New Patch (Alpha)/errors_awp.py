53151132159418f7d03c84fc2d56bb93a60584a8
Lucas Lourenço <dev.lucaslourenco@gmail.com>
2023-08-07 20:20:13 -0300

    Update errors_awp.py

--- New Patch (Alpha)/errors_awp.py
+++ New Patch (Alpha)/errors_awp.py
@@ -2,3 +2,4 @@ class AWPConnectionError(ConnectionError):
     def __init__(self, menssagem="Conexão ao AWP não estabelecida."):
         self.message = menssagem
         super().__init__(self.message)
+        
