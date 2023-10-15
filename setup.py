7ae580ac7bb7811f129b3701a271d676c4a8304f
Lucas Lourenço <dev.lucaslourenco@gmail.com>
2023-10-15 09:12:26 -0300

    att pip

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 with open(r"README.md", "r") as arq:
     readme = arq.read()
 
-setup(name='allwhatspy',
-    version='0.2.00.5',
+setup(name='allwhatspy-awp',
+    version='0.2.00.6',
     license='MIT License',
     author='Lucas Lourenço',
     long_description=readme,
     long_description_content_type="text/markdown",
     author_email='dev.lucaslourenco@gmail.com',
-    keywords='whatsapp api bot mensagem automática analise whatsappAPI allwhatspy awp allwhatspy-awp wrapper message contact analysis github',
+    keywords='whatsapp api bot mensagem automática analise whatsappAPI allwhatspy awp allwhatspy-awp wrapper message contact analysis github automação automacao',
     description=u'Whatsapp Wrapper - Envio de Mensagem Automática - PT-BR. ',
     packages=['AllWhatsPy'],
     install_requires=['webdriver-manager', 'selenium', 'requests'],)
