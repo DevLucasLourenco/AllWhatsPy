05991df26ebd4ef8d8cb0a8c4927ac86d71889cd
Lucas Lourenço <dev.lucaslourenco@gmail.com>
2023-10-26 15:24:25 -0300

    Create business_awp.py

---
+++ AllWhatsPy/business_awp.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class AWPBusiness:
+    
+    def __init__(self) -> None:
+        ...
